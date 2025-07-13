971404e87e0bb32f473e840c28011fd61e47e449
Mei-Mei-only <mei764177@gmail.com>
2025-07-13 20:17:08 +0800

    Update settings.py

--- PetMet/settings.py
+++ PetMet/settings.py
@@ -120,11 +120,7 @@ SESSION_COOKIE_HTTPONLY = True
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default='sqlite:///db.sqlite3',  # Fallback for local development
-        conn_max_age=600
-    )
-}
+    'default': dj_database_url.config( default='postgresql://postgres:postgres@localhost:5432/mysite',        conn_max_age=600    )}
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
