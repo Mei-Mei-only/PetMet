e38b0b3c7d2892eb843ffea879f6a8c08196e7ec
Mei-Mei-only <mei764177@gmail.com>
2025-07-16 16:18:16 +0800

    Update service-worker.js

--- static/service-worker.js
+++ static/service-worker.js
@@ -2,6 +2,12 @@ const CACHE_NAME = 'petmet-cache-v1';
 const urlsToCache = [
   '/',
   '/static/',
+  '/static/PetMetLogo.jpg',
+  '/static/TeamLogo.png',
+  '/static/team/Arvin.png',
+  '/static/team/455955679_1001877174954352_6720492336956922263_n.jpg',
+  '/static/team/455700785_1038882434606928_3900257605003181516_n.jpg',
+  '/static/team/bernalene.png',
   // Add other static files you want to cache
 ];
 
